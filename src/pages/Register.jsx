07aa7bec66a1a9ae191614d55af48195ad926b29
Utkarsh Yadav <95362332+utkarshyadav20@users.noreply.github.com>
2025-04-03 01:32:40 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -149,7 +149,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">WEB Chat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
